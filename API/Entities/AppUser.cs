953311cc057837e69c9b17bf2f6bdbb80110b89a
DTPhu68 <ptcntt@gmail.com>
2025-03-29 14:03:27 +0700

    Walking skeleton API

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+  public class AppUser
+  {
+    public int Id { get; set; }
+
+    [Required, StringLength(100)]
+    public string UserName { get; set; }
+  }
+}
